3f8e3b1e377054b48aad1db35b9172febee48cd0
flor <florcongar26@gmail.com>
2024-05-22 18:25:09 -0600

    Inicial commit

---
+++ src/db.ts
@@ -0,0 +1,30 @@
+import { DataSource } from "typeorm";
+import {User} from '../src/entity/User';
+import { Empleado } from "./entity/empleado";
+import { Area } from "./entity/Area";
+import { Edificio } from "./entity/Edificio";
+import { Vale } from "./entity/vale";
+
+// export const AppDataSourse = new DataSource({
+//     type: 'postgres',
+//     host: 'localhost',
+//     username: 'administrador',
+//     password: 'administrador',
+//     port: 5432,
+//     database: 'db_sice',
+//     entities: [User,Empleado, Edificio, Area, Vale],
+//     logging: true,
+//     synchronize: true
+// })
+
+export const AppDataSourse = new DataSource({
+    type: 'postgres',
+    host: 'roundhouse.proxy.rlwy.net',
+    username: 'postgres',
+    password: 'GgUCDTqEluKqukzOMOuaOGcMyQHgJHhZ',
+    port: 31935,
+    database: 'railway',
+    entities: [User,Empleado, Edificio, Area, Vale],
+    logging: true,
+    synchronize: true
+})
